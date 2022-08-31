bc9a5e89033da939cc9eee42e83e441ebbdfa10b
ARUN KUMAR A <86073278+ArunKumarAgit@users.noreply.github.com>
2022-08-31 22:35:18 +0530

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,16 +4,16 @@ const Contact = () => {
   return (
     <section>
       <h1>Contact</h1>
-      <a href="https://www.linkedin.com/in/kumar-arun-/" target="_blank">
+      <a href="https://www.linkedin.com/in/kumar-arun-/" target="_blank rel="noreferrer" ">
         linkdn
       </a>
-      <a href="https://www.linkedin.com/in/kumar-arun-/" target="_blank">
+      <a href="https://www.linkedin.com/in/kumar-arun-/" target="_blank rel="noreferrer" ">
         facebook
       </a>
-      <a href="https://www.linkedin.com/in/kumar-arun-/" target="_blank">
+      <a href="https://www.linkedin.com/in/kumar-arun-/" target="_blank rel="noreferrer" ">
         github
       </a>
-      <a href="https://www.linkedin.com/in/kumar-arun-/" target="_blank">
+      <a href="https://www.linkedin.com/in/kumar-arun-/" target="_blank rel="noreferrer" ">
         youtube
       </a>
     </section>
